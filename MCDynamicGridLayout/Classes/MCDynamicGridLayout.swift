bf37ac65bf7673e3ac61820f76ec2bb96f0f713e
Mohonish Chakraborty <mohonish.c@gmail.com>
2016-06-30 17:49:39 +0530

    created subclass

---
+++ MCDynamicGridLayout/Classes/MCDynamicGridLayout.swift
@@ -0,0 +1,15 @@
+//
+//  MCDynamicGridLayout.swift
+//  Pods
+//
+//  Created by Mohonish Chakraborty on 30/06/16.
+//
+//
+
+import UIKit
+
+class MCDynamicGridLayout: UICollectionViewLayout {
+    
+    
+
+}
